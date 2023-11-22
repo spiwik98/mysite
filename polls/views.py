223c56ac973b1ea6a3bc71660231e64148eb78ae
spiwik98 <spiwik98@noreply.github.com>
2023-11-22 20:49:58 +0100

    Added context to index function in polls/views.py file

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
@@ -19,4 +23,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
